d188c3a13725cf4d54ccb39715bc36e525c8d352
ShinlaTensei <45889679+ShinlaTensei@users.noreply.github.com>
2022-07-12 20:37:27 +0700

    Update MyItemSceneController.cs

--- Assets/GameAsset/Scripts/Scene Controller/MyItemScene/MyItemSceneController.cs
+++ Assets/GameAsset/Scripts/Scene Controller/MyItemScene/MyItemSceneController.cs
@@ -7,6 +7,9 @@ public class MyItemSceneController : MonoBehaviour
     public GameObject listCarItems;
     public GameObject whereToSpawn;
 
+    [SerializeField] private Transform listShoesItem;
+    [SerializeField] private Transform listBicycleItem;
+
     [Header("Prefab Car")]
     public MyItemSceneItemVehicle carItem;
 
@@ -19,8 +22,21 @@ public class MyItemSceneController : MonoBehaviour
     {
         foreach(var child in ClientData.Instance.ClientVehicle.Vehicles)
         {
-            var item = Instantiate(carItem,whereToSpawn.transform.position, Quaternion.identity,listCarItems.transform);
-            item.SetProperties(child);
+            if (child.BaseStats.NftType == NFTType.Car)
+            {
+                var item = Instantiate(carItem,whereToSpawn.transform.position, Quaternion.identity,listCarItems.transform);
+                item.SetProperties(child);
+            }
+            else if (child.BaseStats.NftType == NFTType.Bicycle)
+            {
+                var item = Instantiate(carItem,Vector3.zero, Quaternion.identity,listBicycleItem.transform);
+                item.SetProperties(child);
+            }
+            else if (child.BaseStats.NftType == NFTType.Shoes)
+            {
+                var item = Instantiate(carItem,Vector3.zero, Quaternion.identity,listShoesItem.transform);
+                item.SetProperties(child);
+            }
         }
     }
 }
