98ca067e4c8fdfe92424c6e59226e37a35c4983a
Trung Dong <trungdongchiwchew@gmail.com>
2022-07-14 13:51:10 +0700

    FixandFillScene

---
+++ Assets/Test/SimpleLocalization/LocalizedforText.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+namespace Assets.SimpleLocalization
+{
+	/// <summary>
+	/// Localize text component.
+	/// </summary>
+    [RequireComponent(typeof(Text))]
+    public class LocalizedforText : MonoBehaviour
+    {
+        public string LocalizationKey;
+
+        public void Start()
+        {
+            Localize();
+            LocalizationManager.LocalizationChanged += Localize;
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.LocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+                GetComponent<Text>().text = LocalizationManager.Localize(LocalizationKey);          
+        }
+    }
+}
