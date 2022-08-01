0bb9672a109aeb892a711e5ae2eee65a8c172abc
Trung Dong <trungdongchiwchew@gmail.com>
2022-08-01 17:30:05 +0700

    update

--- Assets/GameAsset/Scripts/Scene Controller/Gatchascene/random.cs
+++ Assets/GameAsset/Scripts/Scene Controller/Gatchascene/random.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
+using System.Text;
+using System.IO;
+
 public class random : MonoBehaviour
 {
     public TextAsset textAssetData;
@@ -10,12 +13,18 @@ public class random : MonoBehaviour
     public Text idtext;
     public Text nametext;
     public Text pricetext;
+
+
+    public Image ima;
+    public Sprite[] sp;
+
     [Serializable]
     public class Item
     {
         public int id;
         public string name;
         public int price;
+        public string pic;
     }
 
     [Serializable]
@@ -25,21 +34,32 @@ public class random : MonoBehaviour
     }
 
     public ItemList itemListData = new ItemList();
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        ReadCSV();    
+     
+        ReadCSV();
+
     }
-     void Update()
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {          
+        {
             int a = UnityEngine.Random.Range(0, itemListData.items.Length);
-            text.text = a.ToString();
+
+            text.text = (a + 1).ToString();
             idtext.text = (itemListData.items[a].id).ToString();
             nametext.text = (itemListData.items[a].name).ToString();
             pricetext.text = (itemListData.items[a].price).ToString();
+            int num = int.Parse(itemListData.items[a].pic);
+
+            if (String.Compare(num.ToString(), sp[a].name, true) == 0)
+            {
+                ima.sprite = sp[a];
+            }
+
+            remove();
         }
 
     }
@@ -47,17 +67,29 @@ public class random : MonoBehaviour
     {
         string[] data = textAssetData.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
 
-        int Size = data.Length / 3 - 1;
+        int Size = data.Length / 4 - 1;
 
         itemListData.items = new Item[Size];
 
         for (int i = 0; i < Size; i++)
         {
             itemListData.items[i] = new Item();
-            itemListData.items[i].id = int.Parse(data[3 * (i + 1)]);
-            itemListData.items[i].name = data[ 3 * (i + 1 ) + 1 ];
-            itemListData.items[i].price = int.Parse(data[ 3 * (i + 1) + 2]);
+            itemListData.items[i].id = int.Parse(data[4 * (i + 1)]);
+            itemListData.items[i].name = data[4 * (i + 1) + 1];
+            itemListData.items[i].price = int.Parse(data[4 * (i + 1) + 2]);
+            itemListData.items[i].pic = data[4 * (i + 1) + 3];
         }
     }
-    
+    void remove()
+    {
+        if(itemListData.items.Length > 0)
+        {
+            TextWriter tw = new StreamWriter(filename, false);
+            
+            tw = new StreamWriter(filename, true);
+            tw.WriteLine(itemListData.items[1].id);
+        }
+        
+    }
 }
+
