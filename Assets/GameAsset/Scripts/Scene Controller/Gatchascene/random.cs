1476a27963073c66484c3824175a7e783d6e9203
Trung Dong <trungdongchiwchew@gmail.com>
2022-08-03 17:42:49 +0700

    updateSceneOpen

--- Assets/GameAsset/Scripts/Scene Controller/Gatchascene/random.cs
+++ Assets/GameAsset/Scripts/Scene Controller/Gatchascene/random.cs
@@ -9,87 +9,100 @@ using System.IO;
 public class random : MonoBehaviour
 {
     public TextAsset textAssetData;
-    public Text text;
-    public Text idtext;
+
     public Text nametext;
     public Text pricetext;
-
-
     public Image ima;
     public Sprite[] sp;
-
+    public Animator animator;
+    public Animator itemanimator;
+    public AnimationClip clip;
+    public GameObject chestmain;
+    public GameObject caritem;
+    [SerializeField] private Button buttonop;
+    [SerializeField] private Button buttoncl;
     [Serializable]
     public class Item
     {
-        public int id;
+        public string id;
         public string name;
         public int price;
         public string pic;
+
+        public bool Equals(Item other)
+        {
+            if (other == null) return false;
+            return (this.id.Equals(other.id));
+        }
     }
 
-    [Serializable]
-    public class ItemList
-    {
-        public Item[] items;
-    }
-
-    public ItemList itemListData = new ItemList();
+    public List<Item> items = new List<Item>();
 
     // Start is called before the first frame update
     void Start()
     {
-     
         ReadCSV();
-
     }
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            int a = UnityEngine.Random.Range(0, itemListData.items.Length);
 
-            text.text = (a + 1).ToString();
-            idtext.text = (itemListData.items[a].id).ToString();
-            nametext.text = (itemListData.items[a].name).ToString();
-            pricetext.text = (itemListData.items[a].price).ToString();
-            int num = int.Parse(itemListData.items[a].pic);
-
-            if (String.Compare(num.ToString(), sp[a].name, true) == 0)
-            {
-                ima.sprite = sp[a];
-            }
-
-            remove();
-        }
-
-    }
+    //read data
     void ReadCSV()
     {
         string[] data = textAssetData.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
 
         int Size = data.Length / 4 - 1;
 
-        itemListData.items = new Item[Size];
-
         for (int i = 0; i < Size; i++)
         {
-            itemListData.items[i] = new Item();
-            itemListData.items[i].id = int.Parse(data[4 * (i + 1)]);
-            itemListData.items[i].name = data[4 * (i + 1) + 1];
-            itemListData.items[i].price = int.Parse(data[4 * (i + 1) + 2]);
-            itemListData.items[i].pic = data[4 * (i + 1) + 3];
+
+            items.Add(new Item()
+            {
+                id = data[4 * (i + 1)]
+                ,
+                name = data[4 * (i + 1) + 1]
+                ,
+                price = int.Parse(data[4 * (i + 1) + 2])
+                ,
+                pic = data[4 * (i + 1) + 3]
+            });
         }
+
     }
-    void remove()
+
+    public void Open()
     {
-        if(itemListData.items.Length > 0)
-        {
-            TextWriter tw = new StreamWriter(filename, false);
-            
-            tw = new StreamWriter(filename, true);
-            tw.WriteLine(itemListData.items[1].id);
-        }
-        
+        StartCoroutine(wait());
+
+    }
+    IEnumerator wait()
+    {
+        buttoncl.enabled = false;
+        animator.SetBool("opencofirm", true);
+        yield return new WaitForSeconds(3f);
+        animator.SetBool("opencofirm", false);
+
+        caritem.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        itemanimator.SetBool("ok", true);
+
+        int a = UnityEngine.Random.Range(0, items.Count);
+
+        //show
+        nametext.text = (items[a].name).ToString();
+        pricetext.text = (items[a].price).ToString();
+        ima.sprite = sp[int.Parse(items[a].pic)];
+        //remove
+        items.RemoveAt(a);
+
+        yield return new WaitForSeconds(2.5f);
+        animator.SetBool("back", true);
+        chestmain.SetActive(false);
+        buttoncl.enabled = true;
+    }
+    public void close()
+    {
+        chestmain.SetActive(true);
+        caritem.SetActive(false);
+        itemanimator.SetBool("ok", false);
     }
 }
 
